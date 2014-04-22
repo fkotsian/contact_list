4cfed1929dfb73dea55c8e06e6aa4a4c1732d9da
Frank Kotsianas <frankkotsianas@gmail.com>
2014-04-22 10:53:27 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ViralContact::Application.config.secret_key_base = '712e6bac9ad36300e37c908ac4a41ea0a491f18d9e25d5ade7ea08c3156f3b98594564f834f0a11661165252f97394d8d1d2741e8023616e26c1fd4c8ee57726'
